25f181e6baeec2a4b7d8b08b0ec3b087b845b916
ekram <ekruiwc2020@gmail.com>
2024-02-10 22:28:12 +0300

    start

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import './pages/home.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -11,6 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: CreateContractScreen(),
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -31,7 +33,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
---
+++ mobile/lib/utils/constant.dart
@@ -0,0 +1,10 @@
+String infura_url =
+    "https://sepolia.infura.io/v3/9d4f1b1f1d4b4c91b5d2749fd39d7b94";
+
+String owner_private_key =
+    "a35e0038116d3f1853f7e9f40c39107cdcaf7e226ab35ff679279a93118e7a7e";
+
+String voter_private_key =
+    "29b92895fcab1145bfc2edd568c7d5a0824abd16282e9cbc716c00301ad3d624";
+
+String contractAddress1 = "0x6cAA2Ad8c7Ab99C68bC0f3b951E8C4A62802E8be";
